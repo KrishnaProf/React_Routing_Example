e71ae49b6411b9875f8b33fc45cd32617a0ef63c
Leela_Krishna <leela_krishna@dell.com>
2024-02-13 20:52:44 +0530

    React_Routing_Example updated

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { Suspense, lazy } from 'react'
+import {BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom'
+import './App.css'
+const Landing = lazy(() => import('./components/Landing'))
+const Dashboard = lazy(() => import('./components/Dashboard'))
+
+function App() {
+
+  return (
+    <div>  
+        <BrowserRouter>  
+        <Appbar />    
+          <Routes>
+            <Route path="/dashboard" element={<Suspense fallback={"loading..."}><Dashboard /></Suspense>} />
+            <Route path="/" element={<Suspense fallback={"loading..."}><Landing /></Suspense>} />
+          </Routes>
+      </BrowserRouter>
+
+    </div>
+    
+  )
+}
+
+function Appbar() {
+  const navigate = useNavigate();
+  return (
+    <div style={{backgroundColor: "Blue"}}>
+      <button onClick={() => navigate("/")}>Landing</button>
+      <button onClick={() => navigate("/dashboard")}>Dashboard</button>
+    </div>
+  )
+}
+
+export default App
